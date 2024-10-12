8693f72cb7a92481da213fd00d99236be5fd1e81
Stuart Todd <stuarttodd444@gmail.com>
2024-10-12 10:51:36 +0100

    Remove vendor folder and add .gitignore

--- tests/GreeterTest.php
+++ tests/GreeterTest.php
@@ -11,26 +11,27 @@ class GreeterTest extends TestCase
 {
     private Greeter $greeter;
 
+    #[\Override]
     protected function setUp(): void
     {
         $this->greeter = new Greeter();
     }
 
-    public function testSayHello()
+    public function testSayHello(): void
     {
         $expected = 'Well, hello there!';
         $actual = $this->greeter->sayHello();
         $this->assertEquals($expected, $actual, 'sayHello() should return the expected greeting.');
     }
 
-    public function testSayGoodbye()
+    public function testSayGoodbye(): void
     {
         $expected = 'Errr... Goodbye!';
         $actual = $this->greeter->sayGoodbye();
         $this->assertEquals($expected, $actual, 'sayGoodbye() should return the expected farewell.');
     }
 
-    public function testGreet()
+    public function testGreet(): void
     {
         $expected = 'Well, hello there! Errr... Goodbye!';
         $actual = $this->greeter->greet();
