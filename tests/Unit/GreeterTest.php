73f4368b948804b43f9c7a058338d4fbfd1ba6aa
Stuart Todd <stuarttodd444@gmail.com>
2025-03-14 20:21:03 +0000

    coding standards fix

--- tests/Unit/GreeterTest.php
+++ tests/Unit/GreeterTest.php
@@ -2,20 +2,20 @@
 
 declare(strict_types=1);
 use HalfShellStudios\PackageSample\Greeter;
-beforeEach(function () {
+beforeEach(function (): void {
     $this->greeter = new Greeter();
 });
-test('say hello', function () {
+test('say hello', function (): void {
     $expected = 'Well, hello there!';
     $actual = $this->greeter->sayHello();
     expect($actual)->toEqual($expected, 'sayHello() should return the expected greeting.');
 });
-test('say goodbye', function () {
+test('say goodbye', function (): void {
     $expected = 'Errr... Goodbye!';
     $actual = $this->greeter->sayGoodbye();
     expect($actual)->toEqual($expected, 'sayGoodbye() should return the expected farewell.');
 });
-test('greet', function () {
+test('greet', function (): void {
     $expected = 'Well, hello there! Errr... Goodbye!';
     $actual = $this->greeter->greet();
     expect($actual)->toEqual($expected, 'greet() should return the concatenated hello and goodbye messages.');
